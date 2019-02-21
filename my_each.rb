76a598261248da0f9013069fb35009e07e31deac
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-02-21 10:12:12 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
-  # code here
+#This method should accept an argument of an array and use the `while` loop to iterate over each member of that array, yielding each element contained in the array to a block.
+
+def my_each(array)
+  counter = 0
+    while counter < array.length
+    yield(array[counter])
+    counter += 1
+    end
+    array   #returns the original array
+  end
+
+my_each(['hi', 'hello', 'bye', 'goodbye']) do |word| word
 end
+
+
+
+
